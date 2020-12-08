5eeacfe4fa30079f94df5b270a253b58886e8e23
情雨随风 <876451336@qq.com>
2020-12-08 22:53:31 +0800

    修改商品底部样式

--- src/components/common/footer.vue
+++ src/components/common/footer.vue
@@ -1,61 +1,90 @@
 <template>
-	<view class="app-footer">
-		<view class="left">
-			<button class="u-reset-button item" open-type="contact">
-				<u-icon name="server-fill" :size="40"></u-icon>
+	<view class="app-footer u-border-top">
+		<view class="app-item u-border-bottom u-border-right">
+			<button class="u-reset-button" open-type="contact">
+				<u-icon name="server-fill" :size="38" color="#616b80"></u-icon>
 				<view class="text u-line-1">客服</view>
 			</button>
-			<view class="item car">
-				<u-badge class="car-num" :count="99" type="error" :offset="[-3, -6]"></u-badge>
-				<u-icon name="shopping-cart" :size="40"></u-icon>
-				<view class="text u-line-1">购物车</view>
-			</view>
 		</view>
-		<view class="right">
-			<u-button size="default" :hair-line="false" shape="circle" type="error">加入购物车</u-button>
-			<u-button size="default" :hair-line="false" shape="circle" type="warning">立即购买</u-button>
+		<view class="app-item u-border-bottom u-border-right">
+			<u-badge :count="100" type="error" size="mini" :offset="[5, 2]"></u-badge>
+			<u-icon name="shopping-cart" :size="38" color="#616b80"></u-icon>
+			<view class="text u-line-1">购物车</view>
+		</view>
+		<view class="app-item u-border-bottom u-border-right">
+			<u-icon name="star" :size="38" color="#616b80"></u-icon>
+			<view class="text u-line-1">收藏</view>
+		</view>
+		<view class="app-button u-border-bottom" :style="{ paddingLeft: '20rpx' }">
+			<button class="u-reset-button sub-bottom" :style="{ backgroundColor: app.error }">
+				<text>加入购物车</text>
+			</button>
+		</view>
+		<view class="app-button u-border-bottom" :style="{ paddingRight: '20rpx' }">
+			<button class="u-reset-button sub-bottom" :style="{ backgroundColor: app.warning }">
+				<text>立即购买</text>
+			</button>
 		</view>
 	</view>
 </template>
 
 <script>
 export default {
-	name: 'AppFooter'
+	name: 'AppFooter',
+	data() {
+		return {
+			app: {
+				warning: '#ff9900',
+				error: '#fa3534'
+			}
+		}
+	}
 }
 </script>
 
 <style lang="scss" scoped>
 .app-footer {
 	display: flex;
+	align-items: center;
 	background-color: #ffffff;
-	padding-bottom: constant(safe-area-inset-bottomm, 16rpx);
-	padding-bottom: env(safe-area-inset-bottom, 16rpx);
-	.left {
+	padding-bottom: constant(safe-area-inset-bottomm);
+	padding-bottom: env(safe-area-inset-bottom);
+	position: relative;
+	.app-button {
+		flex: 1;
+		height: 100rpx;
 		display: flex;
-		font-size: 20rpx;
-		margin: 16rpx 0;
-		.item {
-			margin: 0 30rpx;
-			&.car {
-				text-align: center;
-				position: relative;
-				.car-num {
-					position: absolute;
-					top: -10rpx;
-					right: -10rpx;
-				}
-			}
-			&.u-reset-button {
-				margin: 0;
-				padding: 0 30rpx;
-			}
+		align-items: center;
+		padding: 0 8rpx;
+		.sub-bottom {
+			width: 100%;
+			height: 80rpx;
+			line-height: 80rpx;
+			text-align: center;
+			border-radius: 40rpx;
+			font-size: 28rpx;
+			color: #ffffff;
 		}
 	}
-	.right {
-		margin: 16rpx 0;
-		flex: 1;
+	.app-item {
+		width: 110rpx;
+		height: 100rpx;
+		position: relative;
+		font-size: 20rpx;
+		color: #141f33;
 		display: flex;
-		justify-content: space-between;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		.u-reset-button {
+			margin: 0;
+			width: 100%;
+			height: 100%;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+		}
 	}
 }
 </style>
